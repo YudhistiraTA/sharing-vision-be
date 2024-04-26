0b6479ab3008218bcb179adad1efa1c421b38ba3
Yudhistira <yudhistira.tribuana@gmail.com>
2024-04-26 14:34:22 +0700

    Removed Status type

--- model/article.go
+++ model/article.go
@@ -1,33 +1,5 @@
 package model
 
-type Status int
-
-const (
-	Publish Status = iota
-	Draft
-	Trash
-)
-
-func (s Status) String() string {
-	return [...]string{"Publish", "Draft", "Trash"}[s]
-}
-
-func (s Status) MarshalText() ([]byte, error) {
-	return []byte(s.String()), nil
-}
-
-func (s *Status) UnmarshalText(data []byte) error {
-	switch string(data) {
-	case `"Publish"`:
-		*s = Publish
-	case `"Draft"`:
-		*s = Draft
-	case `"Trash"`:
-		*s = Trash
-	}
-	return nil
-}
-
 type Article struct {
 	ID        int    `json:"id,omitempty"`
 	Title     string `json:"title,omitempty" validate:"required,min=20"`
